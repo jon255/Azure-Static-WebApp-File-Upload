5c011d726599735b416403d50da35c498e1aa72c
jon255 <59700939+jon255@users.noreply.github.com>
2025-05-16 21:54:54 -0400

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,13 +1,7 @@
 module.exports = async function (context, req) {
-  context.log('Function called successfully');
-  
   context.res = {
     status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: {
-      message: "Function is working!"
-    }
+    headers: { "Content-Type": "application/json" },
+    body: { message: "API is working!" }
   };
 };
